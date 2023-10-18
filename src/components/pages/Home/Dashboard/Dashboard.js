a97df1f59cb2e34a6feb2fffea9026033b112e7b
MohammadAslamM11M <aslam071595@gmail.com>
2023-10-19 00:31:35 +0530

    Graph component added

--- src/components/pages/Home/Dashboard/Dashboard.js
+++ src/components/pages/Home/Dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import ActionAreaCard from "../../../Card/Card";
+import BarGraph from "../../../BarGraph/BarGraph";
 import styles from "./Dashboard.module.css";
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -17,21 +18,21 @@ const Item = styled(Paper)(({ theme }) => ({
 const Dashboard = () => {
     return (
         <Box sx={{ flexGrow: 1, marginTop: "80px" }}>
-            <Grid container spacing={2}>
+            <Grid container spacing={3}>
                 <Grid item xs={12} className={styles.statsCard}>
                     <ActionAreaCard>xs=8</ActionAreaCard>
                     <ActionAreaCard>xs=8</ActionAreaCard>
                     <ActionAreaCard>xs=8</ActionAreaCard>
                     <ActionAreaCard>xs=8</ActionAreaCard>
                 </Grid>
-                <Grid item xs={12}>
-                    <Item>xs=4</Item>
+                <Grid item xs={8} className={styles.graphCard}>
+                    <BarGraph>xs=8</BarGraph>
+                </Grid>
+                <Grid item xs={4} className={styles.graphCard}>
+                    <ActionAreaCard>xs=4</ActionAreaCard>
                 </Grid>
                 <Grid item xs={12}>
-                    <Item>xs=4</Item>
-                </Grid>
-                <Grid item xs={12}>
-                    <Item>xs=8</Item>
+                    <ActionAreaCard>xs=4</ActionAreaCard>
                 </Grid>
             </Grid>
         </Box>
